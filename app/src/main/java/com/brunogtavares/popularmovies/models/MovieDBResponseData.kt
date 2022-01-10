64fd7575a9caa052b1a7579e7aa46222f0e1f2a0
Bruno G Tavares <bgtavares@Brunos-MBP.lan>
2022-01-10 18:50:18 -0500

    Add models based json from response

---
+++ app/src/main/java/com/brunogtavares/popularmovies/models/MovieDBResponseData.kt
@@ -0,0 +1,8 @@
+package com.brunogtavares.popularmovies.models
+
+data class MovieDBResponseData(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
